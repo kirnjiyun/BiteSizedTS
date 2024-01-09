3e55a84bce51167f844285984d43596d3ac7769c
kirnjiyun <kimjiyunee@naver.com>
2024-01-09 09:34:13 +0900

    오류고쳐야함

--- section3/집합.ts
+++ section3/집합.ts
@@ -1,10 +1,12 @@
 {
+    //업캐스트 허용
     let num1: number = 10;
     let num2: 10 = 10;
     num1 = num2;
 }
 
 {
+    //다운캐스트 거의 허용하지 않는다.
     // let num1: 10 = 10;
     // let num2: number = 10;
     // num1 = num2;
